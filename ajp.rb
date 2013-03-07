fcad311982fce0f76ec46f82ee8a8dfc0562cd0a
Johandry Amador <johandry@me.com>
2013-03-06 21:42:43 -0600

    Fixing merging issues

--- ajp.rb
+++ ajp.rb
@@ -1,39 +1,4 @@
-<<<<<<< HEAD
 #!/usr/bin/env ruby
-
-class Jobs
-  define initialize
-    @collection = []
-  end
-  
-  define add
-    
-end
-
-class Job 
-  define initialize
-    @id   = 0
-    @name = ''
-    @type = ''
-    @box_name = ''
-  end
-end
-
-File.open('alljobs.jil', 'r') do |file|
-  file.readlines.each do |line|
-    if line.match(/insert_job: (\w+)/) and job.name.defined
-      
-      job = Job.new
-      count++
-    end
-      
-    job[:name] = line.match(/insert_job: (\w+)/)
-    job[:box_name] = line.match(/box_name: (\w+)/)
-    puts job
-  end
-end
-=======
-#!/usr/bin/ruby
 
 class Job
 
@@ -122,4 +87,3 @@ end
 
 jobs = Jobs.new("alljobs.jil")
 jobs.toYAML
->>>>>>> Ruby
